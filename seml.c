b117398d0dac48f39f24396147f58def90303b76
Toby Bell <tbell@stanford.edu>
2023-10-09 13:30:17 -0700

    Add links and bold text

--- seml.c
+++ seml.c
@@ -14,10 +14,39 @@ void writeo(char const* data, unsigned len) {
   write(1, data, len);
 }
 
+typedef struct Buf {
+  char* data;
+  unsigned capacity;
+  unsigned size;
+} Buf;
+
+void buf_append(Buf* buf, char const* data, unsigned size) {
+  unsigned cap = buf->capacity;
+  unsigned needed = buf->size + size;
+  if (!cap)
+    cap = size;
+  else
+    while (cap < needed)
+      cap *= 2;
+  buf->data = realloc(buf->data, cap);
+  buf->capacity = cap;
+  memcpy(buf->data + buf->size, data, size);
+  buf->size = needed;
+}
+
+void buf_free(Buf* buf) {
+  free(buf);
+  buf->data = NULL;
+  buf->size = 0;
+}
+
 char in_list = 0;
+char bold = 0;
 char buf[16 * 1024 * 1024];
 int count = 0;
 int ofs = 0;
+Buf cur_link_text = {};
+Buf cur_link_url = {};
 
 enum Mode {
   LineReady,
@@ -27,6 +56,9 @@ enum Mode {
   ParEnd,
   HeadReady,
   HeadStart,
+  LinkText,
+  LinkTextPost,
+  LinkUrl,
 };
 
 enum Mode mode = LineReady;
@@ -68,6 +100,19 @@ void par_end(void) {
 void par_start(void) {
   int checkpoint = ofs;
   while (buf[ofs] != '\n') {
+    if (buf[ofs] == '[') {
+      writeo(&buf[checkpoint], ofs - checkpoint);
+      mode = LinkText;
+      next();
+      tail dispatch();
+    }
+    if (buf[ofs] == '*') {
+      writeo(&buf[checkpoint], ofs - checkpoint);
+      output(bold ? "</strong>" : "<strong>");
+      bold = !bold;
+      next();
+      tail dispatch();
+    }
     if (++ofs >= count) {
       writeo(&buf[checkpoint], count - checkpoint);
       return;
@@ -139,6 +184,54 @@ void line_ready(void) {
   tail dispatch();
 }
 
+void link_text(void) {
+  int checkpoint = ofs;
+  while (buf[ofs] != ']') {
+    if (++ofs >= count) {
+      buf_append(&cur_link_text, &buf[checkpoint], count - checkpoint);
+      return;
+    }
+  }
+  buf_append(&cur_link_text, &buf[checkpoint], ofs - checkpoint);
+  mode = LinkTextPost;
+  next();
+  tail dispatch();
+}
+
+void link_text_post(void) {
+  if (buf[ofs] == '(') {
+    mode = LinkUrl;
+    next();
+    tail dispatch();
+  } else {
+    output("[");
+    writeo(cur_link_text.data, cur_link_text.size);
+    output("]");
+    mode = ParStart;
+    tail dispatch();
+  }
+}
+
+void link_url(void) {
+  int checkpoint = ofs;
+  while (buf[ofs] != ')') {
+    if (++ofs >= count) {
+      buf_append(&cur_link_url, &buf[checkpoint], count - checkpoint);
+      return;
+    }
+  }
+  buf_append(&cur_link_url, &buf[checkpoint], ofs - checkpoint);
+  output("<a href=\"");
+  writeo(cur_link_url.data, cur_link_url.size);
+  output("\">");
+  writeo(cur_link_text.data, cur_link_text.size);
+  output("</a>");
+  cur_link_text.size = cur_link_url.size = 0;
+  mode = ParStart;
+  next();
+  tail dispatch();
+}
+
 void dispatch(void) {
   switch (mode) {
     case LineReady:
@@ -155,6 +248,12 @@ void dispatch(void) {
       tail par_end();
     case HeadStart:
       tail head_start();
+    case LinkText:
+      tail link_text();
+    case LinkTextPost:
+      tail link_text_post();
+    case LinkUrl:
+      tail link_url();
   }
   abort();
 }
